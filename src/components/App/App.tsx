b6344eceb725f81aa2ec98e2b5785de6fbca4c06
David Cho-Lerat <david@cho-lerat.com>
2022-08-28 22:07:53 +0200

    Add tailwindcss config

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,18 @@ import styles from "./App.module.css";
 import logo from "./logo.svg";
 
 export const App = () => (
-  <div className={styles["app"]}>
-    <header className={styles["app-header"]}>
-      <img src={logo} className={styles["app-logo"]} alt="logo" />
+  <div className="text-center">
+    <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-3xl">
+      <img
+        src={logo}
+        className={`h-96 pointer-events-none ${styles["app-logo"]}`}
+        alt="logo"
+      />
       <p>
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
-        className={styles["app-link"]}
+        className="text-blue-400"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
