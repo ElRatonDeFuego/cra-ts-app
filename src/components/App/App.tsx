5dc2415ddd70055ec7c278899afbe5162120e45f
David Cho-Lerat <david@cho-lerat.com>
2022-09-03 21:13:37 +0200

    Use Tailwind for App and CSS Modules for original values not in Tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,12 +5,14 @@ import logo from "./logo.svg";
 
 export const App = () => (
   <div className="text-center">
-    <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-3xl">
+    <header
+      className={`min-h-screen flex flex-col items-center justify-center text-white ${
+        styles["app-header"] ?? /* istanbul ignore next */ ""
+      }`}
+    >
       <img
         src={logo}
-        className={`h-96 pointer-events-none ${
-          styles["app-logo"] ?? /* istanbul ignore next */ ""
-        }`}
+        className={"h-96 pointer-events-none animate-spin-slow"}
         alt="logo"
       />
       <p>
@@ -18,7 +20,7 @@ export const App = () => (
         Edit <code>src/App.tsx</code> and save to reload.
       </p>
       <a
-        className="text-blue-400"
+        className={styles["app-link"] ?? /* istanbul ignore next */ ""}
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      animation: {
+        "spin-slow": "spin 20s linear infinite",
+      },
       keyframes: {
         wiggle: {
           "0%": { opacity: 0, transform: "rotate(-3deg)" },
